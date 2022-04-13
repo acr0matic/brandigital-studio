13eaca862fc18bc173c38bfc9968fd6a3fc5dde8
Nikita <main.acr0matic@gmail.com>
2022-04-14 01:05:24 +0300

    feat: доработан скрипт инвертирования хедера

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,41 +1,74 @@
-
 const header = document.querySelector('#header');
-const headerHeight = header.offsetHeight;
 
-const headerDropdown = header.querySelectorAll('.nav__list .dropdown__header');
+if (header) {
 
-let scrollPosition = window.pageYOffset;
-_.forEach(headerDropdown, (dropdown) => {
+  /*
+  --------------------------------------------------------
+            ПЕРЕМЕННЫЕ И ФУНКЦИИ
+  --------------------------------------------------------
+  */
 
-  // Открытие дропдаунов и закрытие при клике не по ним
-  dropdown.addEventListener('click', () => dropdown.parentNode.classList.toggle(StyleСlass.header.dropdown.open));
-  window.addEventListener('click',
-    (e) => {
-      if (!dropdown.parentNode.contains(e.target)) dropdown.parentNode.classList.remove(StyleСlass.header.dropdown.open);
-    });
+  let heroHeight, headerHeight;
+  const getHeight = (block) => block ? block.clientHeight : null;
 
-  // Скрытие их при скролле
+
+
+  /*
+  --------------------------------------------------------
+              СКРИПТ ВЫПАДАЮЩИХ СПИСКОВ
+  --------------------------------------------------------
+  */
+
+  const headerDropdown = header.querySelectorAll('.nav__list .dropdown__header');
+
+  let scrollPosition = window.pageYOffset;
+  _.forEach(headerDropdown, (dropdown) => {
+
+    // Открытие дропдаунов и закрытие при клике не по ним
+    dropdown.addEventListener('click', () => dropdown.parentNode.classList.toggle(StyleСlass.header.dropdown.open));
+    window.addEventListener('click',
+      (e) => {
+        if (!dropdown.parentNode.contains(e.target)) dropdown.parentNode.classList.remove(StyleСlass.header.dropdown.open);
+      });
+
+    // Скрытие их при скролле
+    window.addEventListener('scroll',
+      () => {
+        scrollPosition = window.pageYOffset;
+        if (scrollPosition >= 150) dropdown.parentNode.classList.remove(StyleСlass.header.dropdown.open);
+      });
+  })
+
+
+
+  /*
+  --------------------------------------------------------
+                СКРИПТ ИНВЕРТИРОВАНИЯ ХЕДЕРА
+  --------------------------------------------------------
+  */
+
+  const heroBlock = document.querySelector('[data-hero]')
+
+  heroHeight = getHeight(heroBlock);
+  headerHeight = getHeight(header);
+
+  // Смена цветовой гаммы хедера
   window.addEventListener('scroll',
     () => {
-      scrollPosition = window.pageYOffset;
-      if (scrollPosition >= 150) dropdown.parentNode.classList.remove(StyleСlass.header.dropdown.open);
+      if (heroHeight) {
+        if (window.scrollY >= heroHeight) header.classList.add(StyleСlass.header.inverted);
+        else header.classList.remove(StyleСlass.header.inverted)
+      }
+
+      else {
+        if (window.scrollY >= headerHeight + 50) header.classList.add(StyleСlass.header.inverted);
+        else header.classList.remove(StyleСlass.header.inverted)
+      }
     });
-})
 
-
-// Смена цветовой гаммы хедера
-const heroBlock = document.querySelector('[data-hero]')
-const heroHeight = heroBlock ? heroBlock.offsetHeight : null;
-
-window.addEventListener('scroll',
-  () => {
-    if (heroHeight) {
-      if (window.scrollY >= heroHeight + headerHeight) header.classList.add(StyleСlass.header.inverted);
-      else header.classList.remove(StyleСlass.header.inverted)
-    }
-
-    else {
-      if (window.scrollY >= headerHeight + 50) header.classList.add(StyleСlass.header.inverted);
-      else header.classList.remove(StyleСlass.header.inverted)
-    }
+  // Обновление значений высот при изменении окна
+  window.addEventListener('resize', () => {
+    heroHeight = getHeight(heroBlock);
+    headerHeight = getHeight(header);
   });
+}
