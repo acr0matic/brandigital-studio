422e8e240423a7ce0549c32027548200ddfa9895
Nikita <main.acr0matic@gmail.com>
2022-04-13 20:26:26 +0300

    feat: верстка "блок с видео"

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -2,7 +2,7 @@ const lazyLoadInstance = new LazyLoad({
   elements_selector: '.lazy__item',
 
   callback_loaded: (trigger) => {
-    const container = trigger.closest('picture');
+    const container = trigger.closest('.lazy');
     const loader = container.querySelector('.preloader');
 
     if (loader) loader.classList.add('preloader--hide');
