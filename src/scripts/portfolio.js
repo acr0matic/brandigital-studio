adfb1e4bc7a45adb163c6d57fd61a19be6b0bcb1
Nikita <main.acr0matic@gmail.com>
2022-04-15 00:12:20 +0300

    feat: верстка "портфолио"

---
+++ src/scripts/portfolio.js
@@ -0,0 +1,83 @@
+const portfolio = document.getElementById('portfolio');
+
+if (portfolio) {
+  let tabName = [];
+  const sliderItems = portfolio.querySelectorAll('[data-title]');
+
+  const tab = new Swiper(portfolio.querySelector('.tab__slider'), {
+    simulateTouch: false,
+    allowTouchMove: false,
+
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    },
+
+    a11y: {
+      enabled: false,
+    },
+
+    pagination: {
+      el: '#portfolio .swiper-pagination',
+      clickable: true,
+
+      renderBullet(index, className) {
+        return `<span class="${className}">${tabName[index]}</span>`;
+      },
+    },
+
+    on: {
+      afterInit(instance) {
+        tabName = _.map(sliderItems, 'dataset.title')
+
+        instance.pagination.render();
+        instance.pagination.update();
+      },
+    },
+  });
+
+  _.forEach(sliderItems, (item) => {
+    const inner = item.querySelector('.inner__slider');
+    const thumbnail = item.querySelector('.thumbnail__slider');
+
+    const thumbnailSlider = new Swiper(thumbnail, {
+      allowTouchMove: false,
+
+      spaceBetween: 30,
+      slidesPerView: 'auto',
+
+      nested: true,
+      slideToClickedSlide: true,
+
+      speed: 500,
+
+      a11y: {
+        enabled: false,
+      },
+    });
+
+    const innerSlider = new Swiper(inner, {
+      simulateTouch: false,
+      allowTouchMove: false,
+
+      spaceBetween: 30,
+
+      nested: true,
+      speed: 500,
+
+      a11y: {
+        enabled: false,
+      },
+
+      navigation: {
+        nextEl: inner.querySelector('.portfolio__next'),
+        prevEl: inner.querySelector('.portfolio__prev'),
+      },
+
+      thumbs: {
+        swiper: thumbnailSlider,
+        autoScrollOffset: 1,
+      },
+    });
+  });
+}
