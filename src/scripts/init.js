ba8d891e58339811aa7910746217eae119a8479e
Nikita <main.acr0matic@gmail.com>
2022-05-16 21:50:53 +0300

    feat: обработка форм

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -19,3 +19,16 @@ const modalTrigger = document.querySelectorAll('[data-micromodal-trigger]');
 _.forEach(modalTrigger, modal => {
   modal.addEventListener('click', () => currentModal = modal.dataset.micromodalTrigger);
 });
+
+const tooltip = tippy('[data-tippy-content]', {
+  allowHTML: true,
+  maxWidth: 320,
+  placement: 'right',
+  theme: 'flat',
+});
+
+if (isTablet) _.forEach(tooltip, (item) => item.props.placement = 'bottom');
+
+
+const forms = document.querySelectorAll('form');
+_.forEach(forms, (form) => new Form(form));
