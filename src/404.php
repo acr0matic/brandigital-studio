c07873a1dc275e1f4d2949855ece024bdd160be3
Nikita <main.acr0matic@gmail.com>
2022-05-20 15:51:33 +0300

    feat: верстка 404 страницы

---
+++ src/404.php
@@ -0,0 +1,30 @@
+<?php include_once('layout/partials/header.php') ?>
+
+<!-- Основной контент -->
+<main id="not-found">
+  <section id="not-found" class="section">
+    <div class="container">
+      <div class="not-found">
+        <div class="not-found__content">
+          <h1 class="not-found__title page__title section__title">Ошибка 404</h1>
+          <p class="not-found__description">
+            Страница не найдена или удалена.
+            <br>
+            Возможно введён не верный адрес в адресной строке.
+          </p>
+          <button class="not-found__button button button-primary">Вернуться назад</button>
+        </div>
+        <!-- /.not-found__content -->
+
+        <div class="not-found__illustartion">
+          <img class="not-found__image image image--contain" src="img/page/not-found/not-found/illustration.svg" alt="">
+        </div>
+        <!-- /.not-found__illustartion -->
+      </div>
+      <!-- /.not-found -->
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once('layout/partials/footer.php') ?>
