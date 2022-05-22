0548e10f1795ad6cf02d6572ab15dac99b24c545
Nikita <main.acr0matic@gmail.com>
2022-05-23 01:01:21 +0300

    feat: страница блога

---
+++ src/blog.php
@@ -0,0 +1,224 @@
+<?php include_once('layout/partials/header.php') ?>
+
+<!-- Основной контент -->
+<main id="blog">
+  <section id="pinned" class="section" data-hero>
+    <div class="container">
+      <div class="pinned">
+        <div class="pinned__content">
+          <h2 class="pinned__title section__title">Тренды в Web-дизайне дизайне</h2>
+          <p class="pinned__description section__text">
+            Расскажем вам о самых свежих трендах в веб дизайне в 2022 г. Рассмотрим интересные кейсы и решения.
+          </p>
+          <div class="pinned__action">
+            <button class="pinned__button button button-outline button-outline--white button-outline--hover__gray">Читать</button>
+          </div>
+          <!-- /.pinned__action -->
+        </div>
+        <!-- /.pinned__content -->
+
+        <div class="pinned__picture lazy">
+          <img data-src="img/page/blog/pinned/picture.jpg" alt="" class="pinned__image image image--cover lazy__item">
+          <img src="img/misc/preloader.svg" alt="" class="preloader">
+        </div>
+        <!-- /.pinned__picture -->
+      </div>
+      <!-- /.pinned -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <section id="blog" class="section">
+    <div class="container">
+      <div class="section__header">
+        <h1 class="page__title section__title">
+          Блог <span>Brand&Digital</span>
+        </h1>
+      </div>
+      <!-- /.section__header -->
+
+      <div class="blog">
+        <div class="blog__wrapper">
+          <article class="blog__item blog-item">
+            <a href="">
+              <picture class="blog-item__picture lazy">
+                <img data-src="img/page/blog/blog/picture.jpg" alt="" class="blog-item__image image image--cover lazy__item">
+                <img src="img/misc/preloader.svg" alt="" class="preloader">
+              </picture>
+              <!-- /.blog-item__picture -->
+            </a>
+
+            <div class="blog-item__content">
+              <h3 class="blog-item__title">
+                <a class="link" href="">
+                  Тренды в Web-дизайне 2022 г.
+                </a>
+              </h3>
+
+              <p class="blog-item__description">
+                Брендировать автомобиль пленкой лучше всего при температуре от 60 до 80 градусов по Фаренгейту.
+              </p>
+
+              <div class="blog-item__action">
+                <button class="blog-item__button button button-outline">Читать</button>
+              </div>
+              <!-- /.blog-item__action -->
+
+              <div class="blog-item__filter blog-filter">
+                <div class="blog-filter__item blog-filter__item--blue">
+                  Брендинг
+                </div>
+                <!-- /.blog-filter__item -->
+
+                <div class="blog-filter__item blog-filter__item--green">
+                  Web-дизайн
+                </div>
+                <!-- /.blog-filter__item -->
+
+                <div class="blog-filter__item blog-filter__item--purple">
+                  Дизайн авто
+                </div>
+                <!-- /.blog-filter__item -->
+
+                <div class="blog-filter__item blog-filter__item--orange">
+                  Логотип
+                </div>
+                <!-- /.blog-filter__item -->
+              </div>
+              <!-- /.blog-item__filter blog-filter -->
+            </div>
+            <!-- /.blog-item__content -->
+          </article>
+          <!-- /.blog__item -->
+
+          <article class="blog__item blog-item">
+            <a href="">
+              <picture class="blog-item__picture lazy">
+                <img data-src="img/page/blog/blog/picture.jpg" alt="" class="blog-item__image image image--cover lazy__item">
+                <img src="img/misc/preloader.svg" alt="" class="preloader">
+              </picture>
+              <!-- /.blog-item__picture -->
+            </a>
+
+            <div class="blog-item__content">
+              <h3 class="blog-item__title">
+                <a class="link" href="">
+                  Тренды в Web-дизайне 2022 г.
+                </a>
+              </h3>
+
+              <p class="blog-item__description">
+                Брендировать автомобиль пленкой лучше всего при температуре от 60 до 80 градусов по Фаренгейту.
+              </p>
+
+              <div class="blog-item__action">
+                <button class="blog-item__button button button-outline">Читать</button>
+              </div>
+              <!-- /.blog-item__action -->
+
+              <div class="blog-item__filter blog-filter">
+                <div class="blog-filter__item blog-filter__item--blue">
+                  Брендинг
+                </div>
+                <!-- /.blog-filter__item -->
+
+                <div class="blog-filter__item blog-filter__item--green">
+                  Web-дизайн
+                </div>
+                <!-- /.blog-filter__item -->
+
+                <div class="blog-filter__item blog-filter__item--purple">
+                  Дизайн авто
+                </div>
+                <!-- /.blog-filter__item -->
+
+                <div class="blog-filter__item blog-filter__item--orange">
+                  Логотип
+                </div>
+                <!-- /.blog-filter__item -->
+
+                <div class="blog-filter__item blog-filter__item--gray">
+                  Иллюстрации
+                </div>
+                <!-- /.blog-filter__item -->
+
+                <div class="blog-filter__item blog-filter__item--red">
+                  Анимации
+                </div>
+                <!-- /.blog-filter__item -->
+              </div>
+              <!-- /.blog-item__filter blog-filter -->
+            </div>
+            <!-- /.blog-item__content -->
+          </article>
+          <!-- /.blog__item -->
+
+          <article class="blog__item blog-item">
+            <a href="">
+              <picture class="blog-item__picture lazy">
+                <img data-src="img/page/blog/blog/picture.jpg" alt="" class="blog-item__image image image--cover lazy__item">
+                <img src="img/misc/preloader.svg" alt="" class="preloader">
+              </picture>
+              <!-- /.blog-item__picture -->
+            </a>
+
+            <div class="blog-item__content">
+              <h3 class="blog-item__title">
+                <a class="link" href="">
+                  Тренды в Web-дизайне 2022 г.
+                </a>
+              </h3>
+
+              <p class="blog-item__description">
+                Брендировать автомобиль пленкой лучше всего при температуре от 60 до 80 градусов по Фаренгейту.
+              </p>
+
+              <div class="blog-item__action">
+                <button class="blog-item__button button button-outline">Читать</button>
+              </div>
+              <!-- /.blog-item__action -->
+
+              <div class="blog-item__filter blog-filter">
+                <div class="blog-filter__item blog-filter__item--cyan">
+                  Упаковка
+                </div>
+                <!-- /.blog-filter__item -->
+
+                <div class="blog-filter__item blog-filter__item--gray">
+                  Иллюстрации
+                </div>
+                <!-- /.blog-filter__item -->
+
+                <div class="blog-filter__item blog-filter__item--red">
+                  Анимации
+                </div>
+                <!-- /.blog-filter__item -->
+              </div>
+              <!-- /.blog-item__filter blog-filter -->
+            </div>
+            <!-- /.blog-item__content -->
+          </article>
+          <!-- /.blog__item -->
+        </div>
+        <!-- /.blog__wrapper -->
+
+        <div class="blog__pagination">
+          <a class="link current" href="">1</a>
+          <a class="link" href="">2</a>
+          <a class="link" href="">3</a>
+          <a class="link" href="">4</a>
+          <span>...</span>
+          <a class="link" href="">10</a>
+        </div>
+        <!-- /.blog__pagination -->
+      </div>
+      <!-- /.blog -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <?php include_once('layout/block/callback.php') ?>
+
+</main>
+
+<?php include_once('layout/partials/footer.php') ?>
