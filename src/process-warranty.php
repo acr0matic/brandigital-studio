76c00a882aa4bfd6b4901dd0e4b77a20183d0ee5
Nikita <main.acr0matic@gmail.com>
2022-05-26 19:25:16 +0300

    feat: верстка хедера "процессов"

---
+++ src/process-warranty.php
@@ -0,0 +1,22 @@
+<?php include_once('layout/partials/header.php') ?>
+
+<!-- Основной контент -->
+<main id="warranty">
+  <?php $current = "warranty" ?>
+  <?php include_once('layout/partials/process/header.php') ?>
+
+  <section id="warranty" class="section">
+    <div class="container">
+      <div class="warranty">
+
+      </div>
+      <!-- /.warranty -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <?php include_once('layout/block/callback.php') ?>
+
+</main>
+
+<?php include_once('layout/partials/footer.php') ?>
