98afaae53dbcf5071f7c23195b0d82cd5315a9af
Nikita <main.acr0matic@gmail.com>
2022-06-22 13:52:57 +0300

    feat: главная страница процессов

---
+++ src/process-main.php
@@ -0,0 +1,160 @@
+<?php include_once('layout/partials/header.php') ?>
+
+<!-- Основной контент -->
+<main id="process">
+  <?php include_once('layout/partials/process/header.php') ?>
+
+  <section id="main" class="section">
+    <div class="container">
+      <div class="process">
+        <div class="process__main process-main">
+          <div class="row">
+            <div class="col-12 col-md-6">
+              <h2 class="process-main__title page__title section__title">
+                Процессы компаниии B&D
+              </h2>
+            </div>
+            <!-- /.col-12 col-md-6 -->
+
+            <div class="col-12 col-md-6">
+              <p class="process-main__text section__text">
+                Для нас самое главное - решение бизнес задачи, которую должен решать правильный дизайн. Начиная от технического задания и брифа, заканчивая всеми правками и завершением проекта - мы будем сопровождать вас и стараться приложить все усилия, пока вы не скажете “вау!”
+              </p>
+            </div>
+            <!-- /.col-12 col-md-6 -->
+          </div>
+          <!-- /.row -->
+
+          <div class="process-main__separator"></div>
+        </div>
+        <!-- /.col-5 -->
+
+        <div class="process__body process-body">
+          <div class="process__textblock process-textblock">
+            <div class="row align-items-center justify-content-between">
+              <div class="col-12 col-md-6 col-lg-5">
+                <div class="process-textblock__wrapper">
+                  <div class="process-textblock__number">
+                    1
+                  </div>
+                  <!-- /.process-textblock__number -->
+
+                  <div class="process-textblock__content">
+                    <h3 class="process-textblock__title">Управление проектами</h3>
+                    <p class="process-textblock__description">
+                      Чтобы сделать качественный и выполняющий свои задачи дизайн - мы проведем с Вами консультацию и подробно изучим техническое задание.
+                      <br>
+                      Далее ваш персональный менеджер будет сопровождать вас на протяженнии всего проекта.
+                    </p>
+
+                    <div class="process-textblock__action">
+                      <a href="process-management.php" class="process-textblock__button button button-primary">Как мы ведём проекты</a>
+                    </div>
+                    <!-- /.process-textblock__action -->
+                  </div>
+                  <!-- /.process-textblock__content -->
+                </div>
+                <!-- /.process-textblock__wrapper -->
+              </div>
+              <!-- /.col-5 -->
+
+              <div class="col-12 col-md-6">
+                <picture class="process-textblock__picture lazy">
+                  <img data-src="img/page/process/main/1.svg" alt="" class="process-textblock__image image image--contain lazy__item">
+                  <img src="img/misc/preloader.svg" alt="" class="preloader">
+                </picture>
+              </div>
+              <!-- /.col-12 col-md-6 -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.process__textblock process-textblock -->
+
+          <div class="process__textblock process-textblock">
+            <div class="row flex-row-reverse align-items-center justify-content-between">
+              <div class="col-12 col-md-6 col-lg-5">
+                <div class="process-textblock__wrapper">
+                  <div class="process-textblock__number">
+                    2
+                  </div>
+                  <!-- /.process-textblock__number -->
+
+                  <div class="process-textblock__content">
+                    <h3 class="process-textblock__title">Разработка дизайна</h3>
+                    <p class="process-textblock__description">
+                      Дизайн имеет свою последовательность работы. Мы постоянно поддерживаем связь с клиентами, чтобы следовать определенному курсу. В процессе мы следуем от большего к идеальному единственному, чтобы Вы получили готовое решение для усиления Вашего бизнеса и увеличения продаж и силы бренда.
+                    </p>
+
+                    <div class="process-textblock__action">
+                      <a href="process-design.php" class="process-textblock__button button button-primary">Как мы делаем дизайн</a>
+                    </div>
+                    <!-- /.process-textblock__action -->
+                  </div>
+                  <!-- /.process-textblock__content -->
+                </div>
+                <!-- /.process-textblock__wrapper -->
+              </div>
+              <!-- /.col-5 -->
+
+              <div class="col-12 col-md-6">
+                <picture class="process-textblock__picture lazy">
+                  <img data-src="img/page/process/main/2.svg" alt="" class="process-textblock__image image image--contain lazy__item">
+                  <img src="img/misc/preloader.svg" alt="" class="preloader">
+                </picture>
+              </div>
+              <!-- /.col-12 col-md-6 -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.process__textblock process-textblock -->
+
+          <div class="process__textblock process-textblock">
+            <div class="row align-items-center justify-content-between">
+              <div class="col-12 col-md-6 col-lg-5">
+                <div class="process-textblock__wrapper">
+                  <div class="process-textblock__number">
+                    3
+                  </div>
+                  <!-- /.process-textblock__number -->
+
+                  <div class="process-textblock__content">
+                    <h3 class="process-textblock__title">Рекомендации и гарантии</h3>
+                    <p class="process-textblock__description">
+                      Лучшее подтвержение наших компетенций - довольные партнёры и клиенты. За годы работы таких накопилось большое колличество, стандарты качества работы мы выдерживаем в каждом нашем проекте.
+                    </p>
+
+                    <div class="process-textblock__action">
+                      <a href="process-warranty.php" class="process-textblock__button button button-primary">Наши гарантии</a>
+                    </div>
+                    <!-- /.process-textblock__action -->
+                  </div>
+                  <!-- /.process-textblock__content -->
+                </div>
+                <!-- /.process-textblock__wrapper -->
+              </div>
+              <!-- /.col-5 -->
+
+              <div class="col-12 col-md-6">
+                <picture class="process-textblock__picture lazy">
+                  <img data-src="img/page/process/main/3.svg" alt="" class="process-textblock__image image image--contain lazy__item">
+                  <img src="img/misc/preloader.svg" alt="" class="preloader">
+                </picture>
+              </div>
+              <!-- /.col-12 col-md-6 -->
+            </div>
+            <!-- /.row -->
+          </div>
+          <!-- /.process__textblock process-textblock -->
+        </div>
+        <!-- /.process__body process-body -->
+      </div>
+      <!-- /.warranty -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <?php include_once('layout/block/callback.php') ?>
+
+</main>
+
+<?php include_once('layout/partials/footer.php') ?>
