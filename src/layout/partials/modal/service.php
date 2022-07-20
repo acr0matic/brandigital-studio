9c584d85bed67e307d593f384010ed9a824cd2bf
Nikita <main.acr0matic@gmail.com>
2022-07-21 01:39:19 +0300

    feat: верстка модалки для услуг

--- src/layout/partials/modal/service.php
+++ src/layout/partials/modal/service.php
@@ -1,4 +1,4 @@
-<div class="modal modal--window modal-callback micromodal-slide" id="modal-service" aria-hidden="true">
+<div class="modal modal--window modal-service micromodal-slide" id="modal-service" aria-hidden="true">
   <div class="modal__overlay" data-micromodal-close>
     <div class="modal__container" role="dialog">
       <header class="modal__header">
@@ -12,57 +12,54 @@
       </header>
 
       <main class="modal__content">
-        <p class="modal__text">
-          Оставьте ваши контактные данные и мы свяжемся с вами, удобным для вас способом
-        </p>
+        <picture class="modal__picture">
+          <img src="img/misc/modal/service/picture.png" alt="" class="modal__image image image--contain">
+        </picture>
 
         <div class="modal__wrapper">
-          <form novalidate action="php/mail.php" class="modal__form form form-modal" data-target="callback" data-subject="Рассчитать цену проекта" data-additional="">
-            <div class="input mb-2 mb-lg-3">
-              <input data-required placeholder="Имя" autocomplete="off" type="text" name="user_name" class="input__field input__field--modal input__field--required">
-              <img data-tippy-content="Имя не указано или содержит ошибки" class="input__warning" src="img/icons/misc/warning.svg" alt="">
-            </div>
-            <!-- /.input -->
+          <p class="modal__text custom-scrollbar custom-scrollbar--white">
+            Лендинг (от англ. landing page — «посадочная страница») — это одностраничный сайт с краткой информацией о товаре, услуге или мероприятии. Его задача — превращать посетителей в клиентов.
+          </p>
 
-            <div class="input mb-2 mb-lg-3">
+          <ul class="list modal__list">
+            <li class="list__item">
+              <img class="icon" src="img/icons/component/modal/service/time.svg" alt="">
+              <span class="text">2 недели</span>
+            </li>
+
+            <li class="list__item">
+              <img class="icon" src="img/icons/component/modal/service/ruble.svg" alt="">
+              <span class="text">15 000 руб.</span>
+            </li>
+
+            <li class="list__item">
+              <img class="icon" src="img/icons/component/modal/service/team.svg" alt="">
+              <span class="text">
+                3D-designer
+                <br>
+                Middle-designer
+              </span>
+            </li>
+          </ul>
+        </div>
+        <!-- /.modal__wrapper -->
+
+        <form novalidate action="php/mail.php" class="modal__form form form-modal" data-target="callback" data-subject="Рассчитать цену проекта" data-additional="">
+          <div class="form__wrapper me-5">
+            <div class="input">
               <input data-required placeholder="+7 ХХХ ХХХ-ХХ-ХХ" autocomplete="off" type="tel" name="user_phone" class="input__field input__field--modal input__field--required">
               <img data-tippy-content="Телефон не указан или содержит ошибки" class="input__warning" src="img/icons/misc/warning.svg" alt="">
             </div>
             <!-- /.input -->
 
-            <div class="input mb-2">
-              <input placeholder="E-mail" autocomplete="off" type="email" name="user_email" class="input__field input__field--modal">
-              <img data-tippy-content="Почта содержит ошибки" class="input__warning" src="img/icons/misc/warning.svg" alt="">
-            </div>
-            <!-- /.input -->
+            <div class="form__label mt-1">Свяжемся с вами в Telegram или WhatsApp</div>
+          </div>
+          <!-- /.form__wrapper -->
 
-            <div class="form__info mb-3 mb-lg-4">Обязательные поля</div>
+          <button data-sending="Отправка..." class="modal__button form__button button button-primary" type="submit">Начать проект</button>
+        </form>
 
-            <div class="input input--box mb-2 mb-lg-3">
-              <textarea rows="3" placeholder="Комментарий" name="user_message" class="input__field input__field--modal input__field--box"></textarea>
-            </div>
-            <!-- /.input -->
-
-            <div class="form__action">
-              <div class="form__privacy">
-                <label class="checkbox mb-3">
-                  <input type="checkbox">
-                  <span class="checkbox__mark"></span>
-                  <span class="checkbox__label">Согласен с <a href="privacy.php" class="checkbox__policy link">Политикой обработки данных</a></span>
-                </label>
-              </div>
-              <!-- /.form__privacy -->
-
-              <button data-sending="Отправка..." disabled class="modal__button form__button button button-primary mt-1" type="submit">Отправить</button>
-            </div>
-            <!-- /.form__action -->
-          </form>
-
-          <picture class="modal__picture">
-            <img src="img/misc/modal/callback/picture.svg" alt="" class="modal__image image image--contain">
-          </picture>
-        </div>
-        <!-- /.modal__wrapper -->
+        <div class="modal__circle"></div>
       </main>
     </div>
   </div>
